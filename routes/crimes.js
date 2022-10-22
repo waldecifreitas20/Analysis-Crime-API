b0f352437c5b4ebd74afbd492faff72fc12ec612
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-10-22 14:59:39 -0300

    sub routes removed

--- routes/crimes.js
+++ routes/crimes.js
@@ -1,10 +1,6 @@
-const router = require('express').Router();
-
 const controller = require('../app/controllers/crimes');
 
-router.get('/fill-database', controller.fillDatabase);
-router.get('/search', controller.search);
-
 module.exports = function(app) {
-    app.use('/crimes', router);
+    app.get('/fill-database', controller.fillDatabase);
+    app.get('/search', controller.search);
 }
