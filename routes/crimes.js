51b4f84db54392080f37632e98a38934f75de6dc
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-10-22 16:02:20 -0300

    routes refactored

--- routes/crimes.js
+++ routes/crimes.js
@@ -2,9 +2,17 @@ const controller = require('../app/controllers/crimes');
 const routes = require('./routes');
 
 module.exports = function (app) {
-
+    // Use this route if your database is empty
     app.get(routes.fillDatabase, controller.fillDatabase);
-    app.get(routes.crimes.allCrimes, controller.allCrimes);
-    app.get(routes.crimes.especificCrime, controller.especificCrime);
+    
+    //routes for searching only one crime
+    app.get(routes.crimes.allCrimesYear, controller.allCrimes);
+    app.get(routes.crimes.allCrimesMonth, controller.allCrimes);
+    app.get(routes.crimes.allCrimesAtPeriod, controller.allCrimes);
+    
+    //routes for searching all crimes
+    app.get(routes.crimes.crimeYear, controller.especificCrime);
+    app.get(routes.crimes.crimeMonth, controller.especificCrime);
+    app.get(routes.crimes.crimeAtPeriod, controller.especificCrime);
 
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,14 @@
 module.exports = {
     crimes : {
-        especificCrime : '/especific-crime',
-        allCrimes : '/all-crimes'
+        
+        crimeYear : '/crime/in-year',
+        crimeMonth : '/crime/in-month',
+        crimeAtPeriod : '/crime/at-period',
+
+        allCrimesYear : '/all-crimes/in-year',
+        allCrimesMonth : '/all-crimes/in-month',
+        allCrimesAtPeriod : '/all-crimes/at-period',
+    
     },
     fillDatabase : '/fill-database'
 }
