b1fc206aea9557f300b1b12a45d7f48e03337a49
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-10-24 16:07:52 -0300

    get all by month and year

--- app/services/crime_services.js
+++ app/services/crime_services.js
@@ -1,5 +1,45 @@
-module.exports = {
-    searchCrimes: async function(params) {
-        
+const repository = require('../repositories/crimes_repository');
+
+const MONTH_SEARCH = 0;
+const PERIOD_SEARCH = 1;
+const GENERIC_SEARCH = 2;
+
+const getSearchMode = searchParams => {
+
+    if (searchParams.month != undefined) {
+        return MONTH_SEARCH;
     }
+    if (!!searchParams.period.start || !!searchParams.period.end) {
+        return PERIOD_SEARCH;
+    }
+
+    return GENERIC_SEARCH;
+}
+
+
+module.exports = {
+    searchEspecificCrime: async function (params) {
+        return {
+            status: 200,
+            response: getSearchMode(params)
+        };
+    },
+
+    searchAll: async function (params) {
+        let response;
+
+        const searchMode = getSearchMode(params);
+
+        if (searchMode == MONTH_SEARCH) {
+            response = await repository.getAllOfMonth(params.month, params.year);
+        } else if (searchMode == GENERIC_SEARCH) {
+            response = await repository.getAllOfYear(params.year);
+        }
+
+        return {
+            status: 200,
+            crimes: response
+        };
+    },
+
 }
