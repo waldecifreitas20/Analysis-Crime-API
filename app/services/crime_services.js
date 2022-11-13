ee43b9ce17fb0b84793a33b04bff69ed9a8f9e31
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-11-13 19:49:35 -0300

    period added to param

--- app/services/crime_services.js
+++ app/services/crime_services.js
@@ -36,7 +36,7 @@ module.exports = {
     searchAll: async function (period) {
         const response = await repository.getAll(period);
 
-        const crimeStats = stats.getAllCrimeStats(response);
+        const crimeStats = stats.getAllCrimeStats(response, period);
 
         return {
             status: 200,
