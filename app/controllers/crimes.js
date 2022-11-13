4f1addf469e6d0a359ab7aa49a44fd270fb915cf
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-11-13 11:16:48 -0300

    controller for fill database

--- app/controllers/crimes.js
+++ app/controllers/crimes.js
@@ -1,28 +1,37 @@
 const services = require('../services/crime_services');
+const extenalServices = require('../services/extenals');
 
 
 module.exports = {
-    fillDatabase: async function(req, res) {
-        return res.status(200).send({url : req.url});
+    fillDatabase: async function (req, res) {
+        const year = req.query.year;
+        const type = req.query.type;
+
+        extenalServices.fillDatabase(year, type);
+
+        return res.status(200).send({ message: 'Request created. It has already filling the database' });
     },
-    
-    searchCrimes : async function(req, res) {
+
+    searchCrimes: async function (req, res) {
         const searchParams = {
-            category: req.params.category,
+            onlyDomestic: req.query.domestic,
+            type: req.params.type,
             year: req.query.year || 2021,
-            month : req.query.month,
-            period : {
-                start : req.query.start_month,
-                end : req.query.end_month,
+            month: req.query.month,
+            period: {
+                start: req.query.start_month,
+                end: req.query.end_month,
             }
         }
 
         let response;
 
-        if (!searchParams.category) {
-            response = await services.searchAll(searchParams);
-        } else {
+        const IS_ESPECIFIC_CRIME_SEARCH = searchParams.category;
+        
+        if (IS_ESPECIFIC_CRIME_SEARCH) {
             response = await services.searchEspecificCrime(searchParams);
+        } else {
+            response = await services.searchAll(searchParams);
         }
 
         return res.status(200).send(response);
