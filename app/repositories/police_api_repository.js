742c17ab710217b3d837b27eac866ef0334d4f6c
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-11-13 19:48:53 -0300

    unecessary loop removed

--- app/repositories/police_api_repository.js
+++ app/repositories/police_api_repository.js
@@ -7,8 +7,6 @@ module.exports = {
     getCrimes: async function (year = 2022, crimeType) {
         API_URL = `${API_URL}?year=${year}&&$limit=300000&&$where=primary_type like '${crimeType}'`;
 
-     
-
         console.log('Request opened');
         console.log('Reaching Chigago API for crimes data');
         let crimes = [];
