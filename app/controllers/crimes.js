1d231176c48c9f3dbe2b687f997dcfb9c63625fd
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-11-13 16:00:05 -0300

    controllers beta

--- app/controllers/crimes.js
+++ app/controllers/crimes.js
@@ -9,31 +9,31 @@ module.exports = {
 
         extenalServices.fillDatabase(year, type);
 
-        return res.status(200).send({ message: 'Request created. It has already filling the database' });
+        return res.status(200).send({ message: 'Request created. It has already been filling the database' });
     },
 
-    searchCrimes: async function (req, res) {
+    advancedSearch: async function (req, res) {
         const searchParams = {
             onlyDomestic: req.query.domestic,
-            type: req.params.type,
-            year: req.query.year || 2021,
+            type: req.query.type,
+            year: req.query.year || 2022,
             month: req.query.month,
             period: {
-                start: req.query.start_month,
-                end: req.query.end_month,
+                start: req.query.start_month || '01',
+                end: req.query.end_month || '12',
             }
-        }
+        };
 
-        let response;
-
-        const IS_ESPECIFIC_CRIME_SEARCH = searchParams.category;
-        
-        if (IS_ESPECIFIC_CRIME_SEARCH) {
-            response = await services.searchEspecificCrime(searchParams);
-        } else {
-            response = await services.searchAll(searchParams);
-        }
-
+        let response = await services.advancedSearch(searchParams);
         return res.status(200).send(response);
     },
+
+    allCrimes: async function (req, res) {
+        const period = {
+            start: req.query.start_year || '2019',
+            end: req.query.end_year || '2022',
+        };
+        const response = await services.searchAll(period);
+        return res.status(response.status).send(response);
+    }
 }
