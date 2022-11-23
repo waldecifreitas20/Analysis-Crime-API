b65c9df422abd777a1c41dba89d21b7b70113831
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-11-23 12:05:46 -0300

    url validator

--- app/middlewares/validator.js
+++ app/middlewares/validator.js
@@ -1,15 +1,22 @@
 module.exports = function (req, res, next) {
     const year = req.query.year;
-    const start = req.query.start_month;
-    const end = req.query.end_month;
+    let start = req.query.start_month;
+    let end = req.query.end_month;
 
+    if (!start) {
+        start = '01';
+    }
+
+    if (!end) {
+        end = '12';
+    }
 
     if (start < 1) {
-        start = 1;
+        start = '01';
     }
 
     if (end > 12) {
-        end = 12;
+        end = '12';
     }
 
     if (end < start) {
@@ -19,15 +26,15 @@ module.exports = function (req, res, next) {
     }
 
     if (year == 2022 && end > 10) {
-        end = 10;
+        end = '10';
     }
-    
+
     if (year == 2022 && start > 10) {
-        start = 10;
+        start = '10';
     }
-    
+
     req.query.start_month = start;
     req.query.end_month = end;
-
+    req.query.tyo = '';
     next();
 }
--- routes/crimes.js
+++ routes/crimes.js
@@ -1,11 +1,12 @@
 const controller = require('../app/controllers/crimes');
 const routes = require('./routes');
+const paramsValidator = require('../app/middlewares/validator');
 
 module.exports = function (app) {
-    
-    app.get(routes.advancedSearch, controller.advancedSearch);
-    app.get(routes.allCrimes, controller.allCrimes);
-    
+
+    app.get(routes.advancedSearch, paramsValidator, controller.advancedSearch);
+    app.get(routes.allCrimes, paramsValidator, controller.allCrimes);
+
     // Use this route if your database is empty
     app.get(routes.fillDatabase, controller.fillDatabase);
 }
